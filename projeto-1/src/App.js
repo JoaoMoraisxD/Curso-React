d420a4fbeb6885ea8ce5f53dd237bdd29c96f720
Joao Morais <joaomoraisxd@gmail.com>
2022-12-07 07:32:57 -0300

    Aula React

--- projeto-1/src/App.js
+++ projeto-1/src/App.js
@@ -1,7 +1,16 @@
 import React, { Component }  from 'react';
+import button from './components/button'
 
 function App() {
-  return <div className="App"> Ola </div>;
+  return (
+  <div className="App"> 
+    <h1>Olá React</h1>
+    <button title='Entrar'/>
+    <button title='Sair'/>
+    <button title='Voltar'/>
+  </div>
+  
+  );
 }
 
 export default App;
---
+++ projeto-1/src/components/button/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function button(props) {
+  const { title } = props;
+  
+    return (
+    <button>{ title }</button>
+  );
+}
